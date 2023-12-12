15142972236fd8e3c21cb2986fd5ee8b125ed84c
Allow <106865010+Allowexe@users.noreply.github.com>
2023-12-12 20:34:10 +0100

    Update InitStruct.h

--- InitStruct.h
+++ InitStruct.h
@@ -1,16 +1,17 @@
 #ifndef INIT_STRUCT_H
 #define INIT_STRUCT_H
 
-#include "includeGlobal.h"
-#include "Stuctures.h"
-#include "moteur.h"
 #include "BDD.h"
-#include "InitStruct.h"
+#include "initStruct.h"
+#include "moteur.h"
+#include "Structures.h"
+#include "includeGlobal.h"
 
 void initParking(Parking **p);
 void initPiste(Piste **p);
 void initTaxis(Taxis **t);
-void init(Parking **p, Piste **pi, Taxis **t);
+void initEnVol(EnVol **e);
+void init(Parking **p, Piste **pi, Taxis **t, EnVol **e);
 
 #endif
 
