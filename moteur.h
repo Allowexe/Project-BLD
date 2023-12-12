8e970146edd4b48b1c912865d8c6f2eac0763716
Allow <106865010+Allowexe@users.noreply.github.com>
2023-12-12 20:33:51 +0100

    Update moteur.h

--- moteur.h
+++ moteur.h
@@ -1,29 +1,18 @@
 #ifndef MOTEUR_H
 #define MOTEUR_H
 
-#include "includeGlobal.h"
-#include "Stuctures.h"
-#include "moteur.h"
 #include "BDD.h"
-#include "InitStruct.h"
+#include "initStruct.h"
+#include "Structures.h"
+#include "includeGlobal.h"
 
-void fromParkingToTaxis(Parking **p, Taxis **t, Avion *a, int categorie);
-void testParkingToTaxis(Parking **p, Taxis **t, int timeG);
-//___________________________________________________________________________________________
-void fromTaxisToPiste(Taxis **t, Piste **p, int categorie);
-void testTaxisToPiste(Taxis **t, Piste **p);
-//___________________________________________________________________________________________
-void decollage(Piste **p, EnVol **e, int categorie, int timeG);
-int testDecollage(Piste **p, EnVol **e, int timeG);
-//___________________________________________________________________________________________
-void atterissage(Piste **p, EnVol **e, Avion *a, int categorie);
-void testAtterissage(Parking **p, Piste **pi, EnVol **e, int timeG);
-//___________________________________________________________________________________________
-void fromPisteToParking(Piste **p, Parking **park, int categorie, int timeG);
-//___________________________________________________________________________________________
+void atterissage(EnVol **e, Piste **pi, Parking *p, int categorie, int timeG, int id);
+void testAtterissage(EnVol **e, Piste **pi, Parking *p, int timeG);
+void decollage(Piste **pi, EnVol **e, int timeG);
+void fromParkingToTaxis(Parking **p, Taxis **t, int timeG, int categorie, int id);
+void testFromParkingToTaxis(Parking **p, Taxis **t, int timeG);
+void fromTaxisToPiste(Taxis **t, Piste **pi);
+void fromPisteToParking(Piste **pi, Parking **p, int timeG);
+void boucleMoteur(EnVol **e, Piste **pi, Parking **p, Taxis **t, int timeG);
 
-
-void boucleMoteur(Parking **p, Taxis **t, Piste **pi, EnVol **e, int timeG);
-
-
-#endif
+#endif //MOTEUR_H
