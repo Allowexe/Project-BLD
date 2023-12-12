6b612391288ae422afdf218cd1604eb52e2b4e24
Allow <106865010+Allowexe@users.noreply.github.com>
2023-12-13 00:36:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -24,13 +24,6 @@ int main(int argc, char const *argv[])
         currentTime = time(NULL) - startTime;
         printf("Temps écoulé : %ld secondes\n", currentTime);
         boucleMoteur(&e, &pi, &p, &t, currentTime);
-        printf("Nombre d'avions en vol : %d\n", e->nbAvions);
-        printf("Nombre de gros avions au parking : %d\n", compteAvion(p->premierG));
-        printf("Nombre de moyen avions au parking : %d\n", compteAvion(p->premierM));
-        printf("Nombre de petit avions au parking : %d\n", compteAvion(p->premierP));
-        printf("Nombre de gros avions sur le taxis : %d\n", compteAvion(t->premierG));
-        printf("Nombre de moyen avions sur le taxis : %d\n", compteAvion(t->premierM));
-        printf("Nombre de petit avions sur le taxis : %d\n", compteAvion(t->premierP));
         sleep(1);
         system("clear");
     }
