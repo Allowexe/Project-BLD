f86e089644af1561177fd14e8670e84b39b0532c
Hugo-Scolari <116060608+Hugo-Scolari@users.noreply.github.com>
2023-12-13 02:21:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,33 @@
 #include "BDD.h"
-#include "InitStruct.h"
+#include "initStruct.h"
 #include "moteur.h"
 #include "Structures.h"
 #include "includeGlobal.h"
-#include "affichage.h"
 
 int main(int argc, char const *argv[])
 {
+    animBegining();
+    sleep(1.5);
+    system("clear");
+    int choix = menu();
+    switch (choix)
+    {
+    case 1:
+        InitialisationBdd();
+        break;
+    case 3:
+        exit(0);
+        break;
+    default:
+        break;
+    }
     InitialisationBdd();
     Parking *p = malloc(sizeof(Parking));
     Piste *pi = malloc(sizeof(Piste));
     Taxis *t = malloc(sizeof(Taxis));
     EnVol *e = malloc(sizeof(EnVol));
     init(&p, &pi, &t, &e);
-    time_t startTime = time(NULL);
-    time_t currentTime;
-    animBegining();
-    sleep(1.5);
-    system("clear");
     AssgnBdd(&e, &p);
-    boucleMoteur(&e, &pi, &p, &t, currentTime, startTime);
+    boucleMoteur(&e, &pi, &p, &t);
     return 0;
 }
